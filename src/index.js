a4763e674f7d653ee1d0bc296de26713b258f7dc
Natein <natein@gmail.com>
2020-02-20 22:58:40 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(arguments.length == 0) return 0;
+    let len = array.length;
+    if (len == 0) return 0;
+    let min_value = array[0];
+    for(let i = 1; i < len; i++) {
+        if(array[i] < min_value)
+            min_value = array[i];
+    }    
+    return min_value;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(arguments.length == 0) return 0;
+    let len = array.length;
+    if (len == 0) return 0;
+    let max_value = array[0];
+    for(let i = 1; i < len; i++) {
+        if(array[i] > max_value)
+            max_value = array[i];
+    }    
+    return max_value;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(arguments.length == 0) return 0;
+    let len = array.length;
+    if (len == 0) return 0;
+    let summa = 0;
+    for(let i = 0; i < len; i++) summa += array[i]
+    return summa/len;
 }
